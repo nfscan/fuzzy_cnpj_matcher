e1f06a2a83f489895adfacd13da571b8fc5c23bd
Paulo Miguel Almeida <paulo.ubuntu@gmail.com>
2015-03-18 18:32:15 -0300

    Added a bulk script to generate all valid cnpjs we have so far

--- bulk/generate_cnpj_base.py
+++ bulk/generate_cnpj_base.py
@@ -1,5 +1,17 @@
 __author__ = 'paulo.rodenas'
 
-for cnpj_idx in range(0, 999999999999):
+from models.Cnpj import Cnpj
+
+f = open('cnpjs.txt', 'w')
+
+for cnpj_idx in xrange(0, 999999999999):
     cnpj_zero_padding = str(cnpj_idx).zfill(12)
-    print cnpj_zero_padding
+
+    last_two_digits = Cnpj.calculate_last_digits(cnpj_zero_padding)
+    cnpj_zero_padding += ''.join(last_two_digits)
+
+    assert Cnpj.validate(cnpj_zero_padding)
+
+    f.write(cnpj_zero_padding + '\n')
+
+f.close()
