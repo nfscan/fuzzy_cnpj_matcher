29a561d707fac4fb90eae78a57a978647e9f13ce
Paulo Miguel Almeida <paulo.ubuntu@gmail.com>
2015-03-19 18:42:03 -0300

    Changed the generate_cnpj_base.py to create a bunch of cnpj files instead of one

--- bulk/generate_cnpj_base.py
+++ bulk/generate_cnpj_base.py
@@ -2,22 +2,26 @@ __author__ = 'paulo.rodenas'
 
 from models.Cnpj import Cnpj
 
-f = open('cnpjs_base_10000000.txt', 'w')
 
-for cnpj_idx in xrange(0, 10000000):
-    # cnpj_zero_padding = str(cnpj_idx).zfill(12)
-    # last_two_digits = Cnpj.calculate_last_digits(cnpj_zero_padding)
-    # cnpj_zero_padding += ''.join(last_two_digits)
+for idx in xrange(0, 10):
+    from_idx = idx * 10000000
+    to_idx = (idx + 1) * 10000000
 
-    cnpj_zero_padding = str(cnpj_idx).zfill(8)
-    cnpj_zero_padding += '0001'
-    last_two_digits = Cnpj.calculate_last_digits(cnpj_zero_padding)
-    cnpj_zero_padding += ''.join(last_two_digits)
+    file_name = 'cnpjs_base_' + str(from_idx).zfill(8) + '.txt'
+    print 'Generating from', from_idx, 'to', to_idx, file_name
 
-    # cnpj_zero_padding = Cnpj.generate_valid_cnpj_with_no_branch()
+    f = open(file_name, 'w')
 
-    assert Cnpj.validate(cnpj_zero_padding)
+    for cnpj_idx in xrange(from_idx, to_idx):
 
-    f.write(cnpj_zero_padding + '\n')
+        cnpj_zero_padding = str(cnpj_idx).zfill(8)
+        cnpj_zero_padding += '0001'
+        last_two_digits = Cnpj.calculate_last_digits(cnpj_zero_padding)
+        cnpj_zero_padding += ''.join(last_two_digits)
 
-f.close()
+        # assert Cnpj.validate(cnpj_zero_padding)
+
+        f.write(cnpj_zero_padding + '\n')
+        f.flush()
+
+    f.close()
